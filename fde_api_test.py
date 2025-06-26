45571a27bd4c3d80621974454dccdd645b55558b
serhat23-source <serhat.dogan@appliedtechnology.se>
2025-06-26 09:38:55 +0200

    Changes made

--- fde_api_test.py
+++ fde_api_test.py
@@ -120,4 +120,4 @@ def delete_working_hours(id):
     return jsonify({'message': f'Arbetsrad med id {id} raderad'}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
